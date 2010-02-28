6907f425678a6cb407e125876fdaf2638ba98967
maxhodak <maxhodak@gmail.com>
2010-02-27 20:39:20 -0500

    refactoring to generate the methods rather than hand-specifying the defns

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
                  [clojure-http-client "1.0.0-SNAPSHOT"]
                  [commons-codec "1.3"]
-                 [clj-json "0.1.0-SNAPSHOT"]]
+                 [clj-json "0.2.0-SNAPSHOT"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                     [clj-unit "0.1.0-SNAPSHOT"]
                     [lein-javac "0.0.2-SNAPSHOT"]])
--- src/facebook/client.clj
+++ src/facebook/client.clj
@@ -1,7 +1,8 @@
 (ns facebook.client
   (:require (clojure.contrib [def :as def] [str-utils2 :as string])
             (clojure.http [resourcefully :as resourcefully]
-                          [client :as client]))
+                          [client :as client])
+            (clj-json [core :as json]))
   (:import (org.apache.commons.codec.digest DigestUtils))
   (:gen-class))
 
@@ -10,13 +11,6 @@
 (def rest-endpoint "://api.facebook.com/restserver.php")
 
 (declare *fb-conn*)
-
-(def/defnk make-facebook-connection [api-key secret :secure true :session-key nil]
-  (let [protoc (if (= true secure) protoc-secure protoc-default)]
-    (def *fb-conn* {:endpoint (str protoc rest-endpoint)
-                    :api_key api-key
-                    :secret secret
-                    :session_key session-key})))
 
 (defn md5 [string]
   (DigestUtils/md5Hex string))
@@ -46,10 +40,11 @@
     (assoc p :sig (calculate-signature p (:secret conn)))))
 
 (defn call-method [method params]
-  (let [finalized-params  (finalize-params *fb-conn* method params)]
-    (resourcefully/with-cookies {}
-      (resourcefully/post (:endpoint *fb-conn*)
-                          {} finalized-params))))
+  (let [finalized-params  (finalize-params *fb-conn* method params)
+        response          (resourcefully/with-cookies {}
+                            (resourcefully/post (:endpoint *fb-conn*)
+                                                {} finalized-params))]
+    (json/parse-string (first (:body-seq response)))))
 
 (defn apify-method [call]
   (let [call-sp   (.split call "-")
@@ -59,285 +54,32 @@
       class "."
       (apply string/capitalize method))))
 
+(defmacro define-method [method map args]
+  `(defn ~method []
+     false))
 (comment
-  (defmacro define-method [method args]
-    `(defn ~method ~args
-       (call-method 
-         ~(str "facebook." (apify-method method))
-         {})))
-
-  (define-method my-test-method [query]))
-
-(defn admin-get-allocation []
-  false)
-
-(defn admin-get-app-properties []
-  false)
-
-(defn admin-get-metrics []
-  false)
-
-(defn admin-get-restriction-info []
-  false)
-
-(defn admin-set-app-properties []
-  false)
-
-(defn admin-ban-users []
-  false)
-
-(defn admin-unban-users []
-  false)
-
-(defn admin-get-banned-users []
-  false)
-
-(defn application-get-public-info []
-  false)
-
-(defn auth-create-token []
-  false)
-
-(defn auth-expire-session []
-  false)
-
-(defn auth-get-session []
-  false)
-
-(defn auth-promote-session []
-  false)
-
-(defn auth-revoke-authorization []
-  false)
-
-(defn auth-revoke-extended-permission []
-  false)
-
-(defn batch-run []
-  false)
-
-(defn comments-add []
-  false)
-
-(defn comments-get []
-  false)
-
-(defn comments-remove []
-  false)
-
-(defn connect-get-unconnected-friends-count []
-  false)
-
-(defn connect-register-users []
-  false)
-
-(defn connect-unregister-users []
-  false)
-
-(defn data-get-cookies []
-  false)
-
-(defn data-set-cookie []
-  false)
-
-(defn events-cancel []
-  false)
-
-(defn events-create []
-  false)
-
-(defn events-edit []
-  false)
-
-(defn events-get []
-  false)
-
-(defn events-get-members []
-  false)
-
-(defn events-invite []
-  false)
-
-(defn events-rsvp []
-  false)
-
-(defn fbml-delete-custom-tags []
-  false)
-
-(defn fbml-get-custom-tags []
-  false)
-
-(defn fbml-register-custom-tags []
-  false)
-
-(defn fbml-refresh-img-src []
-  false)
-
-(defn fbml-refresh-ref-url []
-  false)
-
-(defn fbml-setRefHandle []
-  false)
-
-(defn fql-multiquery []
-  false)
-
-(defn fql-query [query]
-  (call-method "facebook.fql.query" {:query query}))
-
-(defn friends-are-friends [uids1 uids2]
-  (call-method "facebook.friends.areFriends"
-    {:uids1 uids1 :uids2 uids2}))
-
-(defn friends-get [uid]
-  (call-method "facebook.friends.get" {:uid uid}))
-
-(defn friends-get-app-users []
-  false)
-
-(defn friends-get-lists []
-  false)
-
-(defn friends-get-mutual-friends []
-  false)
-
-(defn groups-get []
-  false)
-
-(defn groups-get-members []
-  false)
-
-(defn intl-get-translations []
-  false)
-
-(defn intl-upload-native-strings []
-  false)
-
-(defn links-get []
-  false)
-
-(defn links-get-stats []
-  false)
-
-(defn links-post []
-  false)
-
-(defn links-preview []
-  false)
-
-(defn livemessage-send []
-  false)
-
-(defn message-get-threads-in-folder []
-  false)
-
-(defn notes-create []
-  false)
-
-(defn notes-delete []
-  false)
-
-(defn notes-edit []
-  false)
-
-(defn notes-get []
-  false)
-
-(defn notifications-get []
-  false)
-
-(defn notifications-get-list []
-  false)
-
-(defn notifications-mark-read []
-  false)
-
-(defn pages-get-info []
-  false)
-
-(defn pages-is-admin []
-  false)
-
-(defn pages-is-app-added []
-  false)
-
-(defn photos-add-tag []
-  false)
-
-(defn photos-create-album []
-  false)
-
-(defn photos-get []
-  false)
-
-(defn photos-get-albums []
-  false)
-
-(defn photos-get-tags []
-  false)
-
-(defn photos-upload []
-  false)
-
-(defn sms-can-send []
-  false)
-
-(defn sms-send []
-  false)
-
-(defn status-get []
-  false)
-
-(defn status-set []
-  false)
-
-(defn stream-add-comment []
-  false)
-
-(defn stream-add-like []
-  false)
-
-(defn stream-get []
-  false)
-
-(defn stream-get-comments []
-  false)
-
-(defn stream-get-filters []
-  false)
-
-(defn stream-publish []
-  false)
-
-(defn stream-remove []
-  false)
-
-(defn stream-remove-comment []
-  false)
-
-(defn stream-remove-like []
-  false)
-
-(defn users-get-info []
-  false)
-
-(defn users-get-logged-in-user []
-  false)
-
-(defn users-get-standard-info []
-  false)
-
-(defn users-is-app-user []
-  false)
-
-(defn users-is-verified []
-  false)
-
-(defn users-set-status []
-  false)
-
-(defn video-get-upload-limits []
-  false)
-
-(defn video-upload []
-  false)
+     (call-method 
+       ~(format "facebook.%s" map)
+       {}))
+
+(def fb-methods
+  [['admin-get-allocation "admin.getAllocation" []]
+   ['admin-get-app-properties "admin.getAppProperties" []]
+   ['admin-get-metrics "admin.getMetrics" []]
+   ['admin-get-restriction-info "admin.getRestrictionInfo" []]
+   ['admin-set-app-properties "admin.setAppProperties" []]
+   ['admin-ban-users "admin.banUsers" []]
+   ['admin-unban-users "admin.unbanUsers" []]
+   ['admin-get-banned-users "admin.getBannedUsers" []]])
+
+(defn generate-methods []
+  (doseq [method fb-methods]
+    (define-method (first method) (second method) (second (rest method)))))
+
+(def/defnk make-facebook-connection [api-key secret :secure true :session-key nil]
+  (let [protoc (if (= true secure) protoc-secure protoc-default)]
+    (def *fb-conn* {:endpoint (str protoc rest-endpoint)
+                    :api_key api-key
+                    :secret secret
+                    :session_key session-key})
+    (generate-methods)))
